272eee377c95c9c16c137acc5160ddfb34db57ea
bakukats07 <crisvegar2@gmail.com>
2025-02-17 10:13:16 -0500

    Add files via upload

---
+++ plugins/info-totalplugins.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn }) => {
+let totalf = Object.values(global.plugins).filter(
+    (v) => v.help && v.tags
+  ).length;
+conn.reply(m.chat, `*🌷↛ 𝑁𝑢𝑚𝑒𝑟𝑜 𝑡𝑜𝑡𝑎𝑙 𝑑𝑒 𝑚𝑖𝑠 𝑓𝑢𝑛𝑐𝑖𝑜𝑛𝑒𝑠✨* : ${totalf}`,m)
+}
+
+handler.help = ['totalfunciones']
+handler.tags = ['main']
+handler.command = ['totalfunciones', 'comandos', 'funciones']
+handler.register = true
+export default handler 
