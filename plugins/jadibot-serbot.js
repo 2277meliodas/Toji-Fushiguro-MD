6bf442f072153529eaf4398b770e8f61363c2b6e
ⁱᵃᵐ|Lm' Avaricia <sanchezazahel1207@gmail.com>
2025-02-20 08:33:42 -0400

    Update jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -30,8 +30,8 @@ let crm3 = "SBpbmZvLWRvbmFyLmpz"
 let crm4 = "IF9hdXRvcmVzcG9uZGVyLmpzIGluZm8tYm90Lmpz"
 let drm1 = ""
 let drm2 = ""
-let rtx = "♡💐↛𝑀𝑖𝑡𝑠𝑢𝑟𝑖 𝐾𝑎𝑛𝑟𝑜𝑗𝑖-𝑀𝐷↛🍡\n\n✿𝐶𝑜𝑛𝑒𝑥𝑖𝑜𝑛 𝑆𝑢𝑏-𝐵𝑜𝑡 𝑀𝑜𝑑𝑒 𝑄𝑅✿\n\n✿➳ Con otro celular o en la PC escanea este QR para convertirte en un *Sub-Bot* Temporal.\n\n\`1\` » Haga clic en los tres puntos en la esquina superior derecha\n\n\`2\` » Toque dispositivos vinculados\n\n\`3\` » Escanee este codigo QR para iniciar sesion con el bot\n\n✧ ¡Este código QR expira en 45 segundos!."
-let rtx2 = "♡💐↛𝑀𝑖𝑡𝑠𝑢𝑟𝑖 𝐾𝑎𝑛𝑟𝑜𝑗𝑖-𝑀𝐷↛🍡\n\n✿𝐶𝑜𝑛𝑒𝑥𝑖𝑜𝑛 𝑆𝑢𝑏-𝐵𝑜𝑡 𝑀𝑜𝑑𝑒 𝐶𝑜𝑑𝑒✿\n\n✿➳ Usa este Código para convertirte en un *Sub-Bot* Temporal.\n\n\`1\` » Haga clic en los tres puntos en la esquina superior derecha\n\n\`2\` » Toque dispositivos vinculados\n\n\`3\` » Selecciona Vincular con el número de teléfono\n\n\`4\` » Escriba el Código para iniciar sesion con el bot\n\n✧ No es recomendable usar tu cuenta principal."
+let rtx = "∆🥢_*Toji-Fushiguro-MD*_🥢\n\n✿𝐶𝑜𝑛𝑒𝑥𝑖𝑜𝑛 𝑆𝑢𝑏-𝐵𝑜𝑡 𝑀𝑜𝑑𝑒 𝑄𝑅✿\n\n✿➳ Con otro celular o en la PC escanea este QR para convertirte en un *Sub-Bot* Temporal.\n\n\`1\` » Haga clic en los tres puntos en la esquina superior derecha\n\n\`2\` » Toque dispositivos vinculados\n\n\`3\` » Escanee este codigo QR para iniciar sesion con el bot\n\n✧ ¡Este código QR expira en 45 segundos!."
+let rtx2 = "∆🥢_*Toji-Fushiguro-MD*_🥢\n\n✿𝐶𝑜𝑛𝑒𝑥𝑖𝑜𝑛 𝑆𝑢𝑏-𝐵𝑜𝑡 𝑀𝑜𝑑𝑒 𝐶𝑜𝑑𝑒✿\n\n✿➳ Usa este Código para convertirte en un *Sub-Bot* Temporal.\n\n\`1\` » Haga clic en los tres puntos en la esquina superior derecha\n\n\`2\` » Toque dispositivos vinculados\n\n\`3\` » Selecciona Vincular con el número de teléfono\n\n\`4\` » Escriba el Código para iniciar sesion con el bot\n\n✧ No es recomendable usar tu cuenta principal."
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -80,7 +80,7 @@ fs.mkdirSync(pathYukiJadiBot, { recursive: true })}
 try {
 args[0] && args[0] != undefined ? fs.writeFileSync(pathCreds, JSON.stringify(JSON.parse(Buffer.from(args[0], "base64").toString("utf-8")), null, '\t')) : ""
 } catch {
-conn.reply(m.chat, `💐 Use correctamente el comando » ${usedPrefix + command} code`, m)
+conn.reply(m.chat, `🍷 Use correctamente el comando » ${usedPrefix + command} code`, m)
 return
 }
 
