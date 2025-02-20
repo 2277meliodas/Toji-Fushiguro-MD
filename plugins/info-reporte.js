192f25ae09cd9ba2c499c319ea5c5d56797e88b9
ⁱᵃᵐ|Lm' Avaricia <sanchezazahel1207@gmail.com>
2025-02-19 23:01:32 -0400

    Update info-reporte.js

--- plugins/info-reporte.js
+++ plugins/info-reporte.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) return conn.reply(m.chat, '🍡 *Ingrese el error que desea reportar.*', m)
-    if (text.length < 10) return conn.reply(m.chat, '🍡 *Especifique bien el error, mínimo 10 caracteres.*', m)
-    if (text.length > 1000) return conn.reply(m.chat, '🍡 *Máximo 1000 caracteres para enviar el error.*', m)
+    if (!text) return conn.reply(m.chat, '🥢 *Ingrese el error que desea reportar.*', m)
+    if (text.length < 10) return conn.reply(m.chat, '🥢 *Especifique bien el error, mínimo 10 caracteres.*', m)
+    if (text.length > 1000) return conn.reply(m.chat, '🥢 *Máximo 1000 caracteres para enviar el error.*', m)
     const teks = `*✖️ \`R E P O R T E\` ✖️*
 
 ☁️ Número:
@@ -12,7 +12,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
 💬 Mensaje:
 • ${text}`
-    await conn.reply('584120346669@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
+    await conn.reply('584120515006@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
 
     m.reply('⚠️ *El reporte se envío a mi creador, cualquier informe falso puede ocasionar baneo.*')
 }
